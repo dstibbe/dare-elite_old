24dc3eadeff9283beaaae8031d4d37943e8a288f
RBleyenberg <remcobleijenberg@gmail.com>
2019-04-10 08:18:49 +0200

    first

---
+++ src/app/app.routing.ts
@@ -0,0 +1,29 @@
+import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { PagesComponent } from './pages/pages.component';
+import { BlankComponent } from './pages/blank/blank.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
+import { ErrorComponent } from './pages/errors/error/error.component';
+
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: PagesComponent, children: [
+            { path: '', loadChildren: './pages/dashboard/dashboard.module#DashboardModule', data: { breadcrumb: 'Dashboard' } },        
+            { path: 'blank', component: BlankComponent, data: { breadcrumb: 'Blank page' } },
+            { path: 'search', component: SearchComponent, data: { breadcrumb: 'Search' } }
+        ]
+    },
+    { path: 'landing', loadChildren: './pages/landing/landing.module#LandingModule' },
+    { path: 'login', loadChildren: './pages/login/login.module#LoginModule' },
+    { path: 'register', loadChildren: './pages/register/register.module#RegisterModule' },
+    { path: 'error', component: ErrorComponent, data: { breadcrumb: 'Error' } },
+    { path: '**', component: NotFoundComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {
+   preloadingStrategy: PreloadAllModules,  // <- comment this line for activate lazy load
+   // useHash: true
+});
